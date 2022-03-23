5f11d74682e9dc8ba5bcedecd07f4fed2d45095f
Moses Mendoza <mendoza.moses@gmail.com>
2022-03-23 13:44:23 -0700

    Add test that app asks server to listen
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

--- app/src/test/java/numberserver/AppTest.java
+++ app/src/test/java/numberserver/AppTest.java
@@ -4,11 +4,15 @@
 package numberserver;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    ApiServer mockedServer = mock(ApiServer.class);
+
+    @Test
+    void startAsksServerToListen() {
+        App myApp = new App(mockedServer);
+        myApp.start();
+        verify(mockedServer).listen();
     }
 }
