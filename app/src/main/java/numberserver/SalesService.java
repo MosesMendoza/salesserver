9e1fc20f35cb7322c35f47fa51ec48d05bc880a5
Moses Mendoza <mendoza.moses@gmail.com>
2022-03-25 11:01:01 -0700

    Convert value to json on topic for taxservice
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

--- app/src/main/java/numberserver/SalesService.java
+++ app/src/main/java/numberserver/SalesService.java
@@ -7,9 +7,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+import java.util.HashMap;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.stream.*;
+import java.util.Arrays;
 
 public class SalesService extends HttpServlet {
     @Override
@@ -35,10 +40,16 @@ public class SalesService extends HttpServlet {
     private void produceToTopic(String value) {
         final String topicName = "streams-plaintext-input";
         final String key = "sale";
+
+        final Map<String, String> valueDictionary = new HashMap<String, String>();
+
+        valueDictionary.put("amount", value);
+        valueDictionary.put("state_abbreviation", "WA");
+
         Properties props = getProperties();
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
         try {
-            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
+            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, new JSONObject(valueDictionary).toString());
             producer.send(record);
             producer.close();
         }
